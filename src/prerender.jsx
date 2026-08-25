bef4768fa259b72fa3aa2de9845e572362eb45ee
Manuel Ortega <ortegamanuel5622@gmail.com>
2026-08-24 22:55:36 -0300

    Remuevo plugin de prerenderizado para evitar bloqueo en Vercel

--- src/prerender.jsx
+++
@@ -1,8 +0,0 @@
-import { renderToString } from 'react-dom/server'
-import App from './App.jsx'
-
-export function prerender() {
-  return {
-    html: renderToString(<App />)
-  }
-}
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
-import { fileURLToPath } from 'node:url'
-import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { vitePrerenderPlugin } from 'vite-prerender-plugin'
-
-const projectRoot = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-    vitePrerenderPlugin({
-      renderTarget: '#root',
-      prerenderScript: path.join(projectRoot, 'src/prerender.jsx')
-    })
+    tailwindcss()
   ],
   server: {
     port: 3000
